685ce16bc13b5a09bca35894ac67f813a8998502
Adithya Rajendra <92981543+Adithya2406@users.noreply.github.com>
2026-09-10 13:10:27 -0700

    chore: use portable project paths

--- settings.py
+++ settings.py
@@ -1,22 +1,14 @@
+import os
 from pathlib import Path
-import sys
 
-# Sources
-# IMAGE = 'Image'
-# VIDEO = 'Video'
-WEBCAM = 'Webcam'
-RTSP = 'RTSP'
-# YOUTUBE = 'YouTube'
 
-SOURCES_LIST = [RTSP, WEBCAM]
+ROOT = Path(__file__).resolve().parent
 
-# VIDEO_1_PATH = r"D:\BMSIT\Hackathon-fixed\hackathon\hackathon\hack\smoke_smoker_detect\videos\smoker_long4.mp4"
-# VIDEOS_DICT = {
-#     'video_1': VIDEO_1_PATH,
-# }
+IMAGE = "Image"
+WEBCAM = "Webcam"
+RTSP = "Camera stream"
+SOURCES_LIST = [IMAGE, WEBCAM, RTSP]
 
-DETECTION_MODEL = r"C:\Users\adith\Downloads\Team-Whirlpool_24hr_Hackathon\hack\smoke_fire.pt"
-SEGMENTATION_MODEL = r"C:\Users\adith\Downloads\Team-Whirlpool_24hr_Hackathon\hack\smoker_robo.pt"
-
-# Webcam
-WEBCAM_PATH = 0
+SMOKE_MODEL = Path(os.getenv("SMOKE_MODEL_PATH", ROOT / "smoke_fire.pt"))
+SMOKER_MODEL = Path(os.getenv("SMOKER_MODEL_PATH", ROOT / "smoker_robo.pt"))
+WEBCAM_PATH = int(os.getenv("WEBCAM_INDEX", "0"))
